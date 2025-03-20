92e4a701b70595bbd84d2d64271bd4869782cfc0
mohamedElsayedIbrahim <mohamdeesayed@outlook.com>
2025-03-20 17:02:35 +0200

    enable cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\AdminMiddleware;
+use App\Http\Middleware\Cors;
 use App\Http\Middleware\IsArtist;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -20,7 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'admin'=>AdminMiddleware::class,
             'isArtist' => IsArtist::class,
         ]);
-        $middleware->append(HandleCors::class);
+        $middleware->append(Cors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
